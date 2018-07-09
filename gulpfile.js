6ac80ccd59adafafb9b184a150ba559a3ff2bb17
Aman Jha <aman.kr.jha@gmail.com>
2018-07-09 15:29:21 +0530

    Move from CSS to SCSS

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp')
+var sass = require('gulp-sass')
+var browserSync = require('browser-sync')
+
+gulp.task('sass', function () {  
+    gulp.src('src/scss/*.scss')
+        .pipe(sass({includePaths: ['src/scss']}))
+        .pipe(gulp.dest('src/css'));
+});
+
+gulp.task('browser-sync', function() {  
+    browserSync.init(["src/css/*.css", "src/js/*.js"], {
+        server: {
+            baseDir: "src"
+        }
+    });
+});
+
+gulp.task('default', ['sass', 'browser-sync'], function () {  
+    gulp.watch("src/scss/*.scss", ['sass']);
+});
